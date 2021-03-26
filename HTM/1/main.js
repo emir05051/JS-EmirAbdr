e534a0e03ef26c0aebc661e97919ee37979fd239
emir <emir05051@gmail.com>
2021-03-26 21:00:15 +0600

    mastye

--- HTM/1/main.js
+++ HTM/1/main.js
@@ -7,6 +7,27 @@ console.log("befre");
 
 window.addEventListener("load", (event) => {
     console.log(event);
+    console.log([document.body]);
 })
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log(event);
+    console.log([document.body]);
+    console.log([document.body.children[0].children[0].children[0].innerText]);
+    [document.body.children[0].children[0].children[0].innerText] = 'New'
+    document.body.children[0].children[0].innerText = '1'
+
+    console.log([document.getElementById("myid")]);
+
+    console.log(document.getElementsByTagName("div"));
+
+    console.log(Array.from(document.getElementsByTagName("div")).map(n => n.innerHTML));
+
+    console.log(document.querySelectorAll(".b span"));
+
+    const div = document.getElementById("myid");
+    console.log(div.querySelectorAll(".b span"))
+})
+
 
 console.log("after");
