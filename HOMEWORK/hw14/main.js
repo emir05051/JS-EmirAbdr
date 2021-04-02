4a5b80a139470c3d2b2261374ae45f6d5c82eb2f
emir <emir05051@gmail.com>
2021-04-02 23:37:44 +0600

    ьфыеук

--- HOMEWORK/hw14/main.js
+++ HOMEWORK/hw14/main.js
@@ -1,164 +1,138 @@
-// 3. В продолжение яблок
-// 3.1 Первратить ящик Box в полноценный объект с конструктором и прототипом
+  // Переменные как константы . промежуточные переменные 
+  // Функции - в математическом смысле. чистые функция
+  // Объекты - неизменяемые, данные (POJO), Модули - набор функционала (префикс), 
 
-// 3.2 Добавить метод getQuantityString на основании nWord 
+  // React - функционального: компонент - функция
 
-// Первый вариант
-// const appleBox = new Box(["яблоко", "яблока", "яблок"]);
+  // username - обязательное, не меньше 5 символов
+  // passwrods - обязательное, не меньше 8 символов, (содержит символ !) (содержит большую букву и цифру)
+  // age - число, целое, (0, 150)
 
-// Второй вариант
-// const appleBox = new Box(nWord((["яблоко", "яблока", "яблок"]));
+  let validForm = {
+      username: "anton",
+      password: "anton!anton",
+      age: "20",
+      height: "100",
+  };
 
-// appleBox.getQuantityString(10) // 10 яблок
-
-
-// ....
-// const pearBox = new Box(["груша", "груши", "груш"]);
-
-const randomInt = (min, max) =>
-    min + Math.floor(Math.random() * (max - min));
-
-const createArray = (mapFunction = index => index) => length =>
-    Array.from({ length }, (_, index) => mapFunction(index));
-
-const createRandomArray = (min, max) =>
-    createArray(() => randomInt(min, max));
+  let invalidForm = {
+      username: "anto",
+      password: "anto",
+      age: "20a",
+      height: "500"
+  };
 
 
-const analitics = {
-    getPositives: numbers => numbers.filter(n => n > 0),
-    getNegatives: numbers => numbers.filter(n => n < 0),
+  // string -> ValidationState
+  const createValidationState = (value) => ({
+      value, // string - Исходное значение поля
+      result: value, // any - Чистое значение поля
+      isValid: true, //bolean - Флаг валидности поля
+      error: null // string - Ошибка (если есть )
+  });
 
-    getSum: numbers =>
-        numbers.reduce((sum, number) => sum + number, 0),
+  const withError = (state, error) => ({
+      value: state.value,
+      result: state.result,
+      isValid: false,
+      error
+  })
 
-    getAverage: numbers => analitics.getSum(numbers) / numbers.length,
+  // ValidationState -> required -> length(8) -> hasChar(ExclamationMark) -> ValidationState
+  // ValidationState -> required -> isNumber() -> isInRange(0, 150) -> ValidationState
 
-    areAllPositive: numbers => numbers.every(n => n > 0),
+  const required = (state) => {
+      if (!state.isValid) {
+          return state;
+      }
 
-    getAllDivisible: (divisor, numbers) => numbers.filter(n => n % divisor === 0)
-};
+      return state.result.length > 0 ? state : withError(state, "Обязательное поле");
+  }
 
+  const length = (minLength) => (state) => {
+      if (!state.isValid) {
+          return state;
+      }
 
-const nNoun = (form1, form2, form3) => {
-    return (n) => {
+      return state.result.length >= minLength ? state : withError(state, "Длина должна быть не меньше " + minLength);
+  }
 
-        let absN = Math.abs(n);
-
-        // 123456
-        let lastDigt = absN % 10; // 6
-        let secondToLastDigits = Math.trunc(absN / 10) % 10; // 12345 % 10 = 5 
-
-        let word;
-        if (secondToLastDigits === 1) {
-            word = form3;
-        } else {
-            if (lastDigt === 1) {
-                word = form1;
-            } else if (2 <= lastDigt && lastDigt <= 4) {
-                word = form2;
-            } else {
-                word = form3;
-            }
-        }
-
-        return n + " " + word;
-    }
-}
-
-// const nApples = nNoun("яблоко", "яблока", "яблок");
-
-const log = {
-    boxState: box => {
-        console.log("В ящике " + box.getQuantityString(box.amount));
-
-        if (box.amount > 0) {
-            console.log("Полная коробка");
-        } else if (box.amount === 0) {
-            console.log("Пустая коробка");
-        } else { // box < 0
-            console.log("Кредитная коробка");
-        }
-    },
-
-    transaction: (box, amount) => {
-
-        console.log("----------");
-        if (amount >= 0) {
-            console.log("Получили " + box.getQuantityString(amount));
-        } else {
-            console.log("Потеряли " + box.getQuantityString(Math.abs(amount)));
-        }
-    },
-
-    result: (box, goal) => {
-
-        console.log("----------");
-        if (box.amount >= goal) {
-            console.log("Ура мы накопили " + box.getQuantityString(box.amount) + "!");
-        } else {
-            console.log("Не повезло, не фартануло. Мы накопили только " + box.getQuantityString(box.amount) + " =(");
-        }
-    },
-
-    statistics: transactions => {
-        let gains = analitics.getPositives(transactions);
-        let loses = analitics.getNegatives(transactions);
-
-        console.log("Суммарный доход: " + analitics.getSum(gains));
-        console.log("Суммарный расход: " + analitics.getSum(loses));
-
-        console.log("Средний доход: " + analitics.getAverage(gains));
-        console.log("Средний расход: " + analitics.getAverage(loses));
-
-        if (analitics.areAllPositive(transactions)) {
-            console.log("Все транзакции были положительными!");
-        }
-
-        console.log(transactions.map(transaction => transaction >= 0 ? "Доход" : "Расход"));
-
-        console.log("Круглые транзакции", analitics.getAllDivisible(5, transactions));
-    },
-}
-
-class Box {
-    amount;
-
-    constructor(forms, amount = 0) {
-        this.amount = amount;
-        this.getQuantityString = nNoun(...forms);
-    }
-
-    changeAmount(transaction) {
-        this.amount += transaction;
-    }
-
-}
+  const validate = (...validators) => (initialState) =>
+      validators.reduce((state, validator) => validator(state), initialState);
 
 
+  let UserFormValidation = {
+      username: validate(required, length(5)),
+      password: validate(required, length(8), contains("!")),
+      // age: (state) => state,
+      // age: validateAge,
+      // height: validateHeight,
+  };
 
-// ---- 
-let amounts = createRandomArray(-10, 20)(30);
+  const validateFiled = key => value => {
+          const state = createValidationState(value);
 
-let goal = 100;
+          return (
+              UserFormValidation.hasOwnProperty(key) ?
+              UserFormValidation[key](state) :
+              state
+          );
+      }
+      // UserFormValidation[key] ? UserFormValidation[key](value) : UserFormValidation["_default"](value);
 
-let box = new Box(["яблоко", "яблока", "яблок"]);
+  // UserForm = { string: string }
+  // Errors = { string: string }
+  // Maybe<User> = User | null
+  // UserForm -> [boolean, Errors, Maybe<User>]
+  // form -> validateFields -> isFormValid -> 
+  //                        ----------------> [isValid, errors, createUser]
+  // pipe(form, validateFileds, isFormValid, createResult)
+  const validateUserForm = form => {
 
+      const validationStates =
+          Object.entries(form)
+          .map(([key, value]) => [key, validateFiled(key)(value)])
+          .filter(([, state]) => !state.isValid); // [ [key, ValidationState] ]
 
-console.log(box);
-log.boxState(box);
+      const isValid = validationStates.length == 0;
 
-let transactions = [];
+      return [
+          isValid,
+          Object.fromEntries(
+              validationStates.map(([key, state]) => [key, state.error])
+          ),
+          isValid && createUser(form)
+      ];
+  }
 
-while (box.amount < goal && amounts.length > 0) {
-    let amount = amounts.shift();
-    box.changeAmount(amount);
+  const createUser = ({
+      username,
+      password,
+      age
+  }) => ({
+      username,
+      password,
+      age,
+      dateOfRegistration: new Date()
+  });
 
-    transactions.push(amount);
+  // UserForm -> void (Побочный эффект)
+  // Пограничная функция между миром чистых функций и миром побочных эффектов
+  const handleSubmit = (form) => {
+      // проверка типов данных 
+      const [isValid, errors, user] = validateUserForm(form);
 
-    log.transaction(box, amount);
-    log.boxState(box);
-}
+      if (isValid) {
+          // отправить форму 
+          console.log("Создаем пользователя", user);
+      } else {
+          // отображаем ошибки в форме
+          console.log("Форма заполнена с ошибками", errors, form);
+      }
+  }
 
-log.result(box, goal);
-log.statistics(transactions);
+  /// 
+
+  handleSubmit(validForm);
+  console.log("-----");
+  handleSubmit(invalidForm);
