26483083a1e5b3d9566b2661675757c8e19a5c3d
emir <emir05051@gmail.com>
2021-05-23 01:42:53 +0600

    master

---
+++ CanvasHW29/main.js
@@ -0,0 +1,9 @@
+const R = 300;
+const n = 15;
+const a = 2 * Math.PI / n;
+const th = Math.sqrt(3) / 2;
+const tr = Math.sqrt(3) / 3;
+
+window.addEventListener("load", () => {
+    createUi(R, n, a, th, tr)
+  });
